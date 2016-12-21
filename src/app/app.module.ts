b753c4aac6966662605ce236f08e4070fed154be
menghong chhay <chhaymenghong@yahoo.com>
2016-12-20 16:33:26 -0800

    add root routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { appRoutes } from './app.routes';
 
 import { AppComponent } from './app.component';
 import { ArticleComponent } from './article/article.component';
 import { ArticleListComponent } from './article-list/article-list.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+
+// Service
 import { ArticleService } from './article.service';
+import { SourceService } from './source.service';
+import { SearchComponent } from './search/search.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ArticleComponent,
-    ArticleListComponent
+    ArticleListComponent,
+    SidenavComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    appRoutes
   ],
-  providers: [ ArticleService ],
+  providers: [ ArticleService, SourceService ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/app.routes.ts
@@ -0,0 +1,32 @@
+import { Routes, RouterModule } from '@angular/router';
+import { ArticleListComponent } from './article-list/article-list.component';
+import { ModuleWithProviders} from '@angular/core'
+
+// To create route: do the following:
+// 1. Create app.routes.ts file
+// 2. Create variable of type Routes, which is an array
+//  containing all the route in the format of ({path: 'path', component: ComponentName })
+// 3. To create a home route look at the example below
+// 4. export out this route by RouterModule.forRoot(2)
+// 5. In app.module, import this file and add it to the list of imports
+// 6. Add a directive <router-outlet-></router-outlet> as a spot to swap out each component
+// corresponding to each route.
+// 7. Use routerLink on a tag to link a tag to a specific route: ie:
+  // <a routerLink='/news/{{param}}'>
+// 8. Use routerLinkActive='active' to highlight active item
+
+
+const routes : Routes = [
+    // root path
+    {
+      path: '',
+      redirectTo: 'news/reddit-r-all',
+      pathMatch: 'full'
+    },
+    {
+      path : 'news/:sourceKey',
+      component : ArticleListComponent
+    }
+];
+
+export const appRoutes = RouterModule.forRoot( routes );
