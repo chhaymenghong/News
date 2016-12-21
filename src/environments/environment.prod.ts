c15699de4677bd4333fc7b3b2cd49e17e9f28342
menghong chhay <chhaymenghong@yahoo.com>
2016-12-20 19:53:54 -0800

    add list

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  apiKey: 'ea3fe652fcac41fca1918dcd533b2a3b',
-  baseUrl: 'https://newsapi.org/v1/',
-  articlePath: 'articles',
-  sourcePath: 'sources'
-};
